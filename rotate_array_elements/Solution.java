9271416abdfb454de0d2dd8e712b973bfd3c7cdd
pks21g <paulsmith414@yahoo.com>
2023-11-11 23:42:15 -0600

    solution for rotate array elements

--- rotate_array_elements/Solution.java
+++ rotate_array_elements/Solution.java
@@ -1,2 +1,34 @@
-package rotate_array_elements;public class Solution {
+
+class Solution {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+        int k = 5;
+
+        swapElements(arr, 0, arr.length - 1);
+        for (int n : arr)
+            System.out.print(n + " ");
+        System.out.println();
+
+        swapElements(arr, 0, k - 1);
+        for (int n : arr)
+            System.out.print(n + " ");
+        System.out.println();
+
+        swapElements(arr, k, arr.length - 1);
+        for (int n : arr)
+            System.out.print(n + " ");
+        System.out.println();
+
+    }
+
+    public static void swapElements(int[] arr, int st, int ed) {
+        int temp = 0;
+        while (st < ed) {
+            temp = arr[st];
+            arr[st] = arr[ed];
+            arr[ed] = temp;
+            st++;
+            ed--;
+        }
+    }
 }
